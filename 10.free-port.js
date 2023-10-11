1a985bd0987e9eecc90d3789861a31dd01057d41
MateoChacon <xdmateo90@gmail.com>
2023-10-11 15:45:26 -0500

    free ports

---
+++ 10.free-port.js
@@ -0,0 +1,23 @@
+const net = require("node:net");
+
+const findAvailablePort = (desiredPort) => {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+
+    server.listen(desiredPort, () => {
+      const { port } = server.address();
+      server.close(() => {
+        resolve(port);
+      });
+    });
+    server.on("error", (error) => {
+      if (error.code === "EADDRINUSE") {
+        findAvailablePort(0).then((port) => resolve(port));
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
+module.exports = { findAvailablePort };
--- 9.http.js
+++ 9.http.js
@@ -1,10 +1,15 @@
 const http = require("node:http");
+const { findAvailablePort } = require("./10.free-port.js");
+
+const desiredPORT = process.env.PORT ?? 3000;
 
 const server = http.createServer((req, res) => {
   console.log("request received");
   res.end("Hi World!");
 });
 
-server.listen(0, () => {
-  console.log(`server listening on port ${server.address().port}`);
+findAvailablePort(desiredPORT).then((port) => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`);
+  });
 });
